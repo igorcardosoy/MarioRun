776b025ad06f8435e83b2777a15ade7ef2c5fd4e
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-10-31 16:19:59 -0300

    TAD Obstacle, que representa os obstaculos. (Em torno de 15% foi feito)

--- src/include/obstacle.h
+++ src/include/obstacle.h
@@ -4,7 +4,7 @@
 typedef struct obstacle_type* Obstacle;
 
 // Função para inciar o obstaculo, recebe como parametro o renderizador, a posição x e y, a largura, a altura e o tipo. Podendo ser do tipo 1, 2, 3, 4, 5, 6. Ele aloca a memoria para o obstaculo e retorna o mesmo por referencia.
-void obstacle_init(Obstacle* obstacle, SDL_Renderer* renderer, int x, int y, int width, int height, int type);
+void obstacle_init(Obstacle* obstacle, SDL_Renderer* renderer, int width, int height, int type);
 
 // Função para desenhar o obstaculo e ele andar, da direira para esquerda, a uma velocidade que é recibida como parametro, recebe como parametro o renderizador, a tela e o obstaculo. Retorna o obstaculo e fazer com que ele ande.
 void obstacle_animate(Obstacle obstacle, SDL_Renderer* renderer, int width, int height, double speed);
@@ -12,8 +12,14 @@ void obstacle_animate(Obstacle obstacle, SDL_Renderer* renderer, int width, int 
 // Retorna a posição X do obstaculo.
 int obstacle_get_position_x(Obstacle obstacle);
 
+// Retorna a largura do obstaculo.
+int obstacle_get_width(Obstacle obstacle);
+
+// Função para setar a posição X do obstaculo.
+void obstacle_set_position_x(Obstacle obstacle, int x);
+
 // Função para fazer o calculo da colisão do obstaculo.
-void obstacle_get_colision(Obstacle obstacle, int* x1, int* x2, int* y1, int* y2);
+void obstacle_get_colision(Obstacle obstacle, double* x1, double* x2, double* y1, double* y2);
 
 // Função para liberar a memoria alocada para o obstaculo.
 void obstacle_destroy(Obstacle* obstacle);
--- src/lib/obstacle.c
+++ src/lib/obstacle.c
@@ -1,33 +1,55 @@
 #include "../include/obstacle.h"
 
 struct obstacle_type {
-    int x, y;
-    int width, height;
-    SDL_Texture* texture;
+  int x, y;
+  int width, height;
+  SDL_Texture* texture;
 };
 
-void obstacle_init(Obstacle* obstacle, SDL_Renderer* renderer, int x, int y, int width, int height, int type)
+void obstacle_init(Obstacle* obstacle, SDL_Renderer* renderer, int width, int height, int type)
 {
-    *obstacle = malloc(sizeof(struct obstacle_type));
+  *obstacle = malloc(sizeof(struct obstacle_type));
 
-    if (*obstacle != NULL) {
-        (*obstacle)->x = x;
-        (*obstacle)->y = y;
-        (*obstacle)->width = width;
-        (*obstacle)->height = height;
+  if (*obstacle != NULL) {
+    (*obstacle)->x = //not implemented;
+    (*obstacle)->y = //not implemented;
+    (*obstacle)->width = width;
+    (*obstacle)->height = height;
 
-        char path[50];
-        sprintf(path, "assets/images/obstacle%d.png", type);
-        (*obstacle)->texture = IMG_LoadTexture(renderer, path);
-    } else {
-        printf("Erro ao alocar memoria para o obstaculo!\n");
-    }
+    char path[50];
+    sprintf(path, "assets/images/obstacle%d.png", type);
+    (*obstacle)->texture = IMG_LoadTexture(renderer, path);
+  } else {
+    printf("Erro ao alocar memoria para o obstaculo!\n");
+  }
 }
 
-void obstacle_animate(Obstacle obstacle, SDL_Renderer* renderer, int width, int height, double speed);
+void obstacle_animate(Obstacle obstacle, SDL_Renderer* renderer, int width, int height, double speed)
+{
+  //not implemented
+}
 
-int obstacle_get_position_x(Obstacle obstacle);
+int obstacle_get_position_x(Obstacle obstacle)
+{
+  //not implemented
+}
 
-void obstacle_get_colision(Obstacle obstacle, int* x1, int* x2, int* y1, int* y2);
+void obstacle_set_position_x(Obstacle obstacle, int x)
+{
+  //not implemented
+}
 
-void obstacle_destroy(Obstacle* obstacle);
+int obstacle_get_width(Obstacle obstacle)
+{
+  //not implemented
+}
+
+void obstacle_get_colision(Obstacle obstacle, double* x1, double* x2, double* y1, double* y2)
+{
+  //not implemented
+}
+
+void obstacle_destroy(Obstacle* obstacle)
+{
+  //not implemented
+}
