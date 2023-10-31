967c92a12c73585170cc97e856cd2bab804fa3b8
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-10-31 16:22:29 -0300

    Not implemented, 0%.

--- src/lib/queue.c
+++ src/lib/queue.c
@@ -1,15 +1,30 @@
 #include "../include/queue.h"
 
 struct queue_type {
-    int not_implemented;
+  int not_implemented;
 };
 
-void queue_init(Queue* queue);
+void queue_init(Queue* queue)
+{
+  //not implemented
+}
 
-bool queue_is_full(Queue queue);
+bool queue_is_full(Queue queue)
+{
+  //not implemented
+}
 
-bool queue_is_empty(Queue queue);
+bool queue_is_empty(Queue queue)
+{
+  //not implemented
+}
 
-bool queue_enqueue(Queue queue, Element new_element);
+bool queue_enqueue(Queue queue, Element new_element)
+{
+  //not implemented
+}
 
-Element queue_dequeue(Queue queue);
+Element queue_dequeue(Queue queue)
+{
+  //not implemented
+}
