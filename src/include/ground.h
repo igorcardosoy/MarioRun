730f01d71d28aa7d3ef5bd96b0f8bbe7b6f1e1cb
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-10-31 16:20:59 -0300

    TAD Ground, que representa o chão. Em torno de 85% foi feito.

--- src/include/ground.h
+++ src/include/ground.h
@@ -3,7 +3,7 @@
 typedef struct ground_type* Ground;
 
 // Inicializa o chão (ground) e define sua posição e tamanho.
-void ground_init(Ground* ground, SDL_Renderer* renderer, int x, int y, int width, int height);
+void ground_init(Ground* ground, SDL_Renderer* renderer, int width, int height);
 
 // Anima o chão (ground) com sua velocidade. Sendo que gera um efeito de loop e da direita para a esquerda.
 void ground_animate(Ground ground, SDL_Renderer* renderer, int width, int height, double speed);
--- src/lib/ground.c
+++ src/lib/ground.c
@@ -2,13 +2,39 @@
 
 
 struct ground_type {
-    int x, y;
-    int width, height;
-    SDL_Texture* texture;
+  int x, y;
+  int width, height;
+  SDL_Texture* texture;
 };
 
-void ground_init(Ground* ground, SDL_Renderer* renderer, int x, int y, int width, int height);
+void ground_init(Ground* ground, SDL_Renderer* renderer, int width, int height)
+{
+  *ground = malloc(sizeof(struct ground_type));
+  if (*ground != NULL) {
+    (*ground)->x = //not implemented;
+    (*ground)->y = //not implemented;
+    (*ground)->width = width;
+    (*ground)->height = height;
+    (*ground)->texture = IMG_LoadTexture(renderer, "src/assets/ground.png");
+  }
+}
 
-void ground_animate(Ground ground, SDL_Renderer* renderer, int width, int height, double speed);
+void ground_animate(Ground ground, SDL_Renderer* renderer, int width, int height, double speed)
+{
+  ground->height = height;
+  ground->width = width;
 
-void ground_destroy(Ground* ground);
+  ground->x -= speed;
+  if (ground->x <= -width) {
+    ground->x = width;
+  }
+
+  SDL_Rect rect = { ground->x, ground->y, ground->width * 2, ground->height };
+  SDL_RenderCopy(renderer, ground->texture, NULL, &rect);
+}
+
+void ground_destroy(Ground* ground)
+{
+  SDL_DestroyTexture((*ground)->texture);
+  free(*ground);
+}
