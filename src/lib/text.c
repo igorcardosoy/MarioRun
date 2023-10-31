47d9ecf371492895e2c8f7f1526222e52cc83e9f
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-10-31 14:57:05 -0300

    TAD Text, para geração de diferentes textos

--- src/lib/text.c
+++ src/lib/text.c
@@ -1,15 +1,38 @@
 #include "../include/text.h"
 
 struct text_type {
-    TTF_Font* font;
-    SDL_Surface* surface;
-    SDL_Texture* texture;
-    SDL_Rect rect;
-    SDL_Color color;
+  TTF_Font* font;
+  SDL_Surface* surface;
+  SDL_Texture* texture;
+  SDL_Rect rect;
+  SDL_Color color;
 };
 
-void text_init(Text* text, char* font);
+void text_init(Text* text)
+{
+  if (TTF_Init() < 0) {
+    printf("TTF_Init: %s\n", TTF_GetError());
+    exit(1);
+  }
 
-void text_render(Text text, SDL_Renderer* renderer, int x, int y, int width, int height, char* text_to_render);
+  *text = malloc(sizeof(struct text_type));
+  if (*text != NULL) {
+    (*text)->color = (SDL_Color){ 255, 255, 255, 255 };
+  }
+}
 
-void text_destroy(Text* text);
+void text_render(Text text, SDL_Renderer* renderer, int x, int y, int width, int height, char* font, int size, char* text_to_render)
+{
+  text->rect = (SDL_Rect){ x, y, width, height };
+  text->font = TTF_OpenFont(font, size);
+  text->surface = TTF_RenderText_Solid(text->font, text_to_render, text->color);
+  text->texture = SDL_CreateTextureFromSurface(renderer, text->surface);
+  SDL_RenderCopy(renderer, text->texture, NULL, &text->rect);
+}
+
+void text_destroy(Text* text)
+{
+  SDL_DestroyTexture((*text)->texture);
+  TTF_CloseFont((*text)->font);
+  free(*text);
+}
