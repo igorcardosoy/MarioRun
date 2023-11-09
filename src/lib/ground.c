7a203683319a6cd9f8c4c4a358a26e2ebfd0e359
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-11-09 02:11:47 -0300

    Proporção para os tamanhos e looping, quase perfeito

--- src/lib/ground.c
+++ src/lib/ground.c
@@ -11,11 +11,11 @@ void ground_init(Ground* ground, SDL_Renderer* renderer, int width, int height)
 {
   *ground = malloc(sizeof(struct ground_type));
   if (*ground != NULL) {
-    (*ground)->x = //not implemented;
-    (*ground)->y = //not implemented;
+    (*ground)->x = 0;
+    (*ground)->y = 0;
     (*ground)->width = width;
     (*ground)->height = height;
-    (*ground)->texture = IMG_LoadTexture(renderer, "src/assets/ground.png");
+    (*ground)->texture = IMG_LoadTexture(renderer, "src/assets/images/mario_cenary.png");
   }
 }
 
@@ -25,8 +25,8 @@ void ground_animate(Ground ground, SDL_Renderer* renderer, int width, int height
   ground->width = width;
 
   ground->x -= speed;
-  if (ground->x <= -width) {
-    ground->x = width;
+  if (ground->x <= -width-speed) {
+    ground->x = 0;
   }
 
   SDL_Rect rect = { ground->x, ground->y, ground->width * 2, ground->height };
